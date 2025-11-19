3b652af6e2b7b8806fc6f04fea7a04e2eb6643df
d4rkfl4m3z <129306246+Nemorous@users.noreply.github.com>
2025-11-18 18:56:17 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="wordreaper",
-    version="2.1.0",
+    version="2.2.1",
     author="d4rkfl4m3z",
     description="Reap & Forge Wordlists for Password Cracking",
     long_description=long_description,
