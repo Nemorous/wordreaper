96e3bde7977fa65f3e1d88a51c96b163b6771c58
d4rkfl4m3z <129306246+Nemorous@users.noreply.github.com>
2025-12-13 09:09:44 -0800

    Update text_scraper.py

--- word_reaper/scraper/text_scraper.py
+++ word_reaper/scraper/text_scraper.py
@@ -5,7 +5,7 @@ from tqdm import tqdm
 RED = '\033[91m'
 RESET = '\033[0m'
 
-# Default headers to mimic a real browser (Chrome 120 on Windows)
+# Default headers to mimic a real browser
 DEFAULT_HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
